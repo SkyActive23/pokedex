6fa694274f3e0ee6ce25ddcf1ac5ea97406c19f7
Sky <skyrj50031@maricopa.edu>
2022-02-09 00:31:42 -0700

    Update scripts.js
    Created pokemonList variable of objects with four keys name, height, weight, and type.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+pokemonList[{
+    name: "Squirtle",
+    height: 0.8,
+    weight: 9,
+    type: ["water"]
+},
+
+{
+    name: "Charmander",
+    height: 1.0,
+    weight: 10,
+    type: ["fire"]
+},
+
+{
+    name: "Bulbasaur",
+    height: 0.6,
+    weight: 9,
+    type: ["grass, poison"]
+},
+
+{
+    name: "Pikachu",
+    height: .05,
+    weight: 6,
+    type: ["electirc"]
+}
+]
