42e0bba0a278e4883590d9c429269521ef48792d
Sky <skyrj50031@maricopa.edu>
2022-02-10 16:26:43 -0700

    Update scripts.js
    Updated js to iterate through pokemonList and print each pokemon name, height, and weight

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,39 @@
-pokemonList[{
-    name: "Squirtle",
-    height: 0.8,
-    weight: 9,
-    type: ["water"]
-},
+let pokemonList = [
+    {
+        name: "Squirtle",
+        height: 0.8,
+        weight: 9,
+        type: ["water"]
+    },
 
-{
-    name: "Charmander",
-    height: 1.0,
-    weight: 10,
-    type: ["fire"]
-},
+    {
+        name: "Charmander",
+        height: 1.0,
+        weight: 10,
+        type: ["fire"]
+    },
 
-{
-    name: "Bulbasaur",
-    height: 0.6,
-    weight: 9,
-    type: ["grass, poison"]
-},
+    {
+        name: "Bulbasaur",
+        height: 0.6,
+        weight: 9,
+        type: ["grass, poison"]
+    },
 
-{
-    name: "Pikachu",
-    height: .05,
-    weight: 6,
-    type: ["electirc"]
-}
-]
+    {
+        name: "Pikachu",
+        height: .05,
+        weight: 6,
+        type: ["electirc"]
+    }
+];
 
+// iterates through the pokemonlist Array and finds which pokemon is bigger than 8
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name)
+
+    if (pokemonList[i].weight > 8) {
+        document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ', ' + 'Weight: ' + pokemonList[i].weight + ')' + ' This pokemon is heavy. ');
+    } else {
+        document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ', ' + 'Weight: ' + pokemonList[i].weight + ')' + ' This pokemon is light. ');
+    }
 }
